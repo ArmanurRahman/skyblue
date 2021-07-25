8b8fd92176370d1986c0ffe1125ee97bb41a5cf5
mubeen <mubeenarman19@gmail.com>
2021-07-25 20:25:46 +0900

    implement jwt token

--- go.mod
+++ go.mod
@@ -3,29 +3,21 @@ module github.com/ArmanurRahman/skyblue
 go 1.15
 
 require (
-	github.com/ArmanurRahman/go-app v0.0.0-20210715151516-90d503329338
-	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
-	github.com/cockroachdb/cockroach-go v2.0.1+incompatible // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-chi/chi/v5 v5.0.3
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator v9.31.0+incompatible
-	github.com/gobuffalo/attrs v1.0.0 // indirect
-	github.com/gobuffalo/envy v1.9.0 // indirect
-	github.com/gobuffalo/fizz v1.13.0 // indirect
-	github.com/gobuffalo/flect v0.2.3 // indirect
-	github.com/gobuffalo/genny v0.6.0 // indirect
-	github.com/gobuffalo/logger v1.0.4 // indirect
-	github.com/gobuffalo/nulls v0.4.0 // indirect
-	github.com/gobuffalo/packr/v2 v2.8.1 // indirect
-	github.com/gobuffalo/pop v4.13.1+incompatible // indirect
-	github.com/gobuffalo/pop/v5 v5.3.4 // indirect
-	github.com/gobuffalo/validate v2.0.4+incompatible // indirect
+	github.com/google/uuid v1.3.0
 	github.com/jackc/pgconn v1.9.0
 	github.com/jackc/pgx/v4 v4.12.0
-	github.com/jmoiron/sqlx v1.3.4 // indirect
 	github.com/joho/godotenv v1.3.0
 	github.com/justinas/nosurf v1.1.1
+	github.com/kr/pretty v0.2.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.7.0
 	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e
-	honnef.co/go/tools v0.2.0
+	golang.org/x/tools v0.0.0-20200103221440-774c71fcf114
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
---
+++ internal/token/jwt_maker_test.go
@@ -0,0 +1,70 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/stretchr/testify/require"
+)
+
+func TestJWTMaker(t *testing.T) {
+
+	key := "abcdefghijklmnopqrstuvwxyz125987"
+	maker, err := NewJWTMaker(key)
+	require.NoError(t, err)
+
+	username := "Mubeen"
+	duration := time.Hour
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
+
+}
+
+func TestExpiredJWTToken(t *testing.T) {
+	key := "abcdefghijklmnopqrstuvwxyz125987"
+	maker, err := NewJWTMaker(key)
+	require.NoError(t, err)
+
+	token, err := maker.CreateToken("Mubeen", -time.Hour)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
+
+func TestInvalidJWTTokenAlgNone(t *testing.T) {
+	key := "abcdefghijklmnopqrstuvwxyz125987"
+	key2 := "abcdefghijklmnopqrstuvwxyz1259sd"
+	payload, err := NewPayload(key, time.Minute)
+	require.NoError(t, err)
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodNone, payload)
+	token, err := jwtToken.SignedString(jwt.UnsafeAllowNoneSignatureType)
+	require.NoError(t, err)
+
+	maker, err := NewJWTMaker(key2)
+	require.NoError(t, err)
+
+	payload, err = maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrInvalidToken.Error())
+	require.Nil(t, payload)
+}
